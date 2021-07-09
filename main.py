1601a33b1e0588a7a71d8dc702f2b108c2c9918d
minexixi <minexixifomil@qq.com>
2021-07-09 21:58:54 +0800

    更新-未完成

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# 这是一个示例 Python 脚本。
+import os,argparse
+from PIL import Image
+def Color2RGB(Cname, mB):
+    C = Image.open(Cname)
+    w, h =C .size
+    nR = Image.new('RGBA', C.size, 'Black')
+    nG = Image.new('RGBA', C.size, 'Black')
+    nB = Image.new('RGBA', C.size, 'Black')
+    for x in range(w):
+        for y in range(h):
+            PR, PG, PB, PA = C.getPixel((x, y))
+            nR.PutPixel((x, y), (PR, PR, PR))
+            nG.PutPixel((x, y), (PG, PG, PG))
+            nB.PutPixel((x, y), (PB, PB, PB))
+    Nname = Cname.split('.')
+    del Nname[-1]
+    Nn = '.'.join(Nname)
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+    nR.save(Nn + '_R.png')
+    nG.save(Nn + '_G.png')
+    nB.save(Nn + '_B.png')
+    if mB == '1':
+        nM = Image.new('RGBA', (3 * w, h), 'black')
+        nM.Paste(nR, (0, 0))
+        nM.Paste(nG, (w + 1, 0))
+        nM.Paste(nB, (2 * w + 2, 0))
+        nM.Save(Nn + '_Mix.png')
+    elif mB == '2':
+        nM = Image.new('RGBA', (w, 3 * h), 'black')
+        nM.Paste(nR, (0, 0))
+        nM.Paste(nG, (0, h + 1))
+        nM.Paste(nB, (0, 2 * h + 2))
+        nM.Save(Nn + '_Mix.png')
 
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-
-# 按间距中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+def RGB2Color(Rname, Gname, Bname):
+    R = Image.OPEN(Rname)
+    G = Image.OPEN(Gname)
+    B = Image.OPEN(Bname)
